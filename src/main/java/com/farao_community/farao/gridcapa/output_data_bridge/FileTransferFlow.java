d610e31824dfb3da7b191952efddb95fc44cc2d3
thiriondan <daniel.thirion@rte-france.com>
2025-03-06 15:44:18 +0100

    Fix log
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>

--- src/main/java/com/farao_community/farao/gridcapa/output_data_bridge/FileTransferFlow.java
+++ src/main/java/com/farao_community/farao/gridcapa/output_data_bridge/FileTransferFlow.java
@@ -57,7 +57,7 @@ public class FileTransferFlow {
     IntegrationFlow generateFileTransferFlow(String fromChannel, String toChannel, String fileNamePattern) {
         return IntegrationFlow.from(fromChannel)
                 .filter(Message.class, message -> fileNameMatches(message, fileNamePattern))
-                .log(LoggingHandler.Level.INFO, PARSER.parseExpression("\"File \" + headers." + FILE_NAME_HEADER + " + \" matches expected pattern, transferred to FTP\""))
+                .log(LoggingHandler.Level.INFO, PARSER.parseExpression("\"File \" + headers." + FILE_NAME_HEADER + " + \" matches expected pattern, transferring to FTP\""))
                 .channel(toChannel)
                 .get();
     }
